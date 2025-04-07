32835e50f2471c994d76eac31e3e777883148609
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-04-08 00:42:39 +0300

    Bitirmae

---
+++ BitirmeProjesi-ll/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BitirmeProjesi_ll.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
